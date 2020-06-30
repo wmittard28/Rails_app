f61f609183ebc5f7b94bd7e48da141e8bd3e9380
wmittard28 <billymittard@ymail.com>
2020-06-30 15:02:02 -0400

    Built out sessions and users views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    class ApplicationController < ActionController::Base
+        protect_from_forgery with: :exception
+        helper_method :current_user
+        helper_method :authorized?
+
+        def authorized(user)
+            current_user.id == user.id
+        end
+
+        def current_user
+            @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        end
+
+    end
 end
